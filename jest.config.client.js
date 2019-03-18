6e21ed311551754e9c27dc50223473774657371f
James Olson <sixfootsixdesigns@gmail.com>
2019-03-17 22:10:10 -0600

    cleanup typescript and jest config

--- jest.config.client.js
+++ jest.config.client.js
@@ -1,15 +1,16 @@
+const {defaults} = require('jest-config');
+
 module.exports = {
+  moduleFileExtensions: [
+    ...defaults.moduleFileExtensions,
+    "ts",
+    "tsx"
+  ],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
     "^.+\\.(css|scss|sass)$": "<rootDir>/config/jest/styleMock.js"
   },
   testMatch: [
-    "<rootDir>/src/client/**/?(*.)(spec|test).{js,jsx,ts,tsx}"
-  ],
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx"
+    "<rootDir>/src/client/**/?(*.)(spec|test).{ts,tsx}"
   ]
 }
--- jest.config.server.js
+++ jest.config.server.js
@@ -1,10 +1,20 @@
+const {defaults} = require('jest-config');
+
 module.exports = {
-  preset: "ts-jest",
-  testMatch: [
-    "<rootDir>/src/server/**/?(*.)(spec|test).{js,ts}"
-  ],
   moduleFileExtensions: [
-    "ts",
-    "js"
-  ]
+    ...defaults.moduleFileExtensions,
+    'ts',
+  ],
+  transform: {
+    '^.+\\.(ts)$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.server.json',
+    },
+  },
+  testMatch: [
+    "<rootDir>/src/server/**/?(*.)(spec|test).{ts,tsx}"
+  ],
+  preset: 'ts-jest',
 }
